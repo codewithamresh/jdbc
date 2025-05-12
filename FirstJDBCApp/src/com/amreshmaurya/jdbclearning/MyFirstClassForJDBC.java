57984fbacd21ddb42877363f1d2db660006fe0bb
Amresh Maurya <amreshbinary@gmail.com>
2025-05-12 22:07:16 +0530

    crud using jdbc

--- FirstJDBCApp/src/com/amreshmaurya/jdbclearning/MyFirstClassForJDBC.java
+++ FirstJDBCApp/src/com/amreshmaurya/jdbclearning/MyFirstClassForJDBC.java
@@ -6,52 +6,50 @@ public class MyFirstClassForJDBC {
 
 	public static void main(String[] args) {
 		
-	String url = "mysql://localhost:3306/jdbclearning";
-	String user = "root";
+	String url = "jdbc:mysql://localhost:3306/jdbclearning";
+	String username = "root";
 	String password = "Mysql@001";
-	Connection connection = null;	
-		
-		
-		try {
-			
-		
-		// load the driver
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		// establish connection
-   	    connection = DriverManager.getConnection(url,user,password);
-	     
-   	    Statement statement = connection.createStatement();
-		
-   	    // execute query
-   	    String sql = "INSERT INTO studentinfo (id,name,age,city) VALUES (1,'Amresh',24,'Amethi')";
-   	    
-   	    int rowAffected = statement.executeUpdate(sql);
-   	    
-   	    // process the result
-   	    if(rowAffected == 0) {
-   	    	System.out.println("Failed to insert data");
-   	    }else {
-   	    	System.out.println("Data insert successfully");
-   	    }
-   	    //close the resources
-   	    statement.close();
-   	    connection.close();
-   	    
-		}
-		catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
 
-			e.printStackTrace();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			System.out.print("finally block");
+		// Establish connection
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Connected to the database!");
+
+			// Create a statement
+			Statement stmt = connection.createStatement();
+
+			// create
+			String sql = "INSERT INTO  studentinfo(id,name,age,city) VALUES (3,'Ajay',25,'Rae Bareli');";
+		
+			// update
+			 sql = "UPDATE  studentinfo  SET name='Anoop' WHERE id=3;";
+			 
+		    // delete
+			 sql = "DELETE FROM studentinfo WHERE id=2;";
 			
+			 
+			 //read 
+			 sql = "SELECT * from studentinfo;";
+			 
+//			int rowAffected = stmt.executeUpdate(sql);
+			 
+//		if(rowAffected !=0) {
+//			System.out.println("Data inserted successfully");
+//		}
+		
+		ResultSet rs =	 stmt.executeQuery(sql);
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+" "+ rs.getString(2)+ " " 
+	+ rs.getInt(3) +" "	+ rs.getString(4));
 		}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// This block will be executed after try-catch
+			System.out.println("Finally block executed.");
+		}
+		
+
 		
 
 	}
